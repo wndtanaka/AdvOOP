fb9a32d232bd1ffec91fa5a68c032110b08a06fa
Unknown <wndtanaka@gmail.com>
2018-02-12 16:03:17 +1100

    Week 2 Generics

---
+++ Assets/1-Generics/Scripts/CustomList.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Generics
+{
+    public class CustomList<T>
+    {
+        private T[] data;
+        public int amount;
+
+        public CustomList()
+        {
+            amount = 0;
+        }
+        public void Add(T item)
+        {
+            // Create a new array of amount + 1
+            T[] cache = new T[amount + 1];
+            // Check if the list has been initialized
+            if (data != null)
+            {
+                // copy all existing items to new array
+                for (int i = 0; i < data.Length; i++)
+                {
+                    cache[i] = data[i];
+                }
+            }
+            // set the end element to new item
+            cache[amount] = item;
+            // point to new array
+            data = cache;
+            // increment amount;
+            amount++;
+        }
+    }
+}
---
+++ Assets/1-Generics/Scripts/GenericTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Generics
+{
+    public class GenericTest : MonoBehaviour
+    {
+        public GameObject prefab;
+        public int spawnAmount = 20;
+        public float spawnRadius = 50f;
+        public CustomList<GameObject> gameObjects = new CustomList<GameObject>();
+        // Use this for initialization
+        void Start()
+        {
+            gameObjects.Add();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
